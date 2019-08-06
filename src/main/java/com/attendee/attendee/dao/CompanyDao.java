b1cb0f3fc5aeb63e85772a4f7e61db20d9e765fc
yatogami <reinaldy.ilham@gmail.com>
2019-08-06 09:36:07 +0700

    CompanyDao

--- src/main/java/com/attendee/attendee/dao/CompanyDao.java
+++ src/main/java/com/attendee/attendee/dao/CompanyDao.java
@@ -3,36 +3,62 @@ package com.attendee.attendee.dao;
 import java.util.List;
 import java.util.UUID;
 
-import javax.transaction.Transactional;
-
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.attendee.attendee.model.Company;
 
 @Repository
 public class CompanyDao extends ParentDao {
-	@Transactional
-	public void save(Company company) {
-		super.entityManager.merge(company);
-	}
-	
-	@Transactional
-	public void delete(UUID id) {
-		Company company = findById(id);
-		super.entityManager.remove(company);
-	}
-	
 	@SuppressWarnings("unchecked")
 	@Transactional
 	public Company findById(UUID id) {
 		List<Company> list = super.entityManager
-				.createQuery("FROM Company WHERE id=:id")
+				.createQuery("FROM Company"
+						+ "WHERE id = :id")
 				.setParameter("id", id)
 				.getResultList();
-		if(list.size() == 0) {
+		
+		if (list.size() == 0) {
 			return new Company();
-		} else {
-			return list.get(0);
+		}
+		else {
+			return (Company)list.get(0);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Transactional
+	public Company findByBk(String kode) {
+		List<Company> list = super.entityManager
+				.createQuery("FROM Company"
+						+ "WHERE kode = :kode")
+				.setParameter("kode", kode)
+				.getResultList();
+		
+		if (list.size() == 0) {
+			return new Company();
+		}
+		else {
+			return (Company)list.get(0);
+		}
+	}
+	
+	@Transactional
+	public boolean isIdExist(UUID id) {
+		if(findById(id).getId() == null) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+	
+	@Transactional
+	public boolean isBkExist(String kode) {
+		if(findByBk(kode).getKode() == null) {
+			return false;
+		}else {
+			return true;
 		}
 	}
 	
@@ -45,28 +71,20 @@ public class CompanyDao extends ParentDao {
 		return list;
 	}
 	
-	@SuppressWarnings("unchecked")
 	@Transactional
-	public Company findByBk(String kode) {	
-		
-		List<Company> list = super.entityManager
-                .createQuery("from Company where kode=:kode")
-                .setParameter("kode", kode)
-                .getResultList();
-
-		if (list.size() == 0) {
-			return new Company();
-		}
-		else {
-			return (Company)list.get(0);
-		}
+	public void save(Company company) {
+		super.entityManager.merge(company);
 	}
 	
-	public boolean isBkExist(String kode) {
-		if(findByBk(kode).getId() == null) {
-			return false;
-		} else {
-			return true;
-		}
+	@Transactional
+	public void update(Company company) {
+		super.entityManager.merge(company);
 	}
+	
+	@Transactional
+	public void delete(UUID id) {
+		Company company = findById(id);
+		super.entityManager.remove(company);
+	}
+
 }
