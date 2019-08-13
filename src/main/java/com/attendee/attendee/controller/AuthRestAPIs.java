811cf562372318ffb757adee0bf03a9864def004
ajiprays <ajiprays@users.noreply.github.com>
2019-08-13 15:11:15 +0700

    fix userCompany

--- src/main/java/com/attendee/attendee/controller/AuthRestAPIs.java
+++ src/main/java/com/attendee/attendee/controller/AuthRestAPIs.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,7 @@ import com.attendee.attendee.service.UserService;
  
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+@Controller
 public class AuthRestAPIs {
  
     @Autowired
